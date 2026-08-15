b87b5908c5b7032ca5d91f4d2f19f55ed4a3654d
aidilprmdta <sekaiiprojectmv@gmail.com>
2026-08-15 11:48:01 +0700

    chore(tests): set FIREBASE_WEB_API_KEY for test environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,7 @@ import os
 
 # Auth endpoints require this key; tests mock httpx responses instead of calling Firebase.
 os.environ.setdefault("FIREBASE_WEB_API_KEY", "test-firebase-web-api-key")
+
+from app.core.rate_limit import limiter
+
+limiter.enabled = False
